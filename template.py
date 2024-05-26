186812af23d7da0336a56581f6b65979e75f3780
samiullah <sami606713@gmail.com>
2024-05-27 01:47:05 +0500

    Update template.py

--- template.py
+++ template.py
@@ -1,53 +1,53 @@
 import os
-
+import logging
+logging.basicConfig(level=logging.INFO)
 # Make the folder structure
+project_name="ml_project"
 dirs=[
     os.path.join("data","raw"),
     os.path.join("data","process"),
-    os.path.join("src","components"),
-    os.path.join("src","pipeline"),
+    os.path.join("src",project_name),
+    os.path.join(f"src/{project_name}",'components'),
+    os.path.join(f"src/{project_name}","pipeline"),
     "notebook",
     "models"
 ]
 
 for dir_ in dirs:
-    os.makedirs(dir_,exist_ok=True)
+    if not os.path.exists(dir_):
+        os.makedirs(dir_,exist_ok=True)
 
-    # add a gitkeep file
+        # add a gitkeep file
 
-    git_keep=os.path.join(dir_,".gitkeep")
+        git_keep=os.path.join(dir_,".gitkeep")
 
-    with open(git_keep,"w") as f:
-        pass
+        with open(git_keep,"w") as f:
+            pass
 
 # Make the files
 
 files=[
-    os.path.join("src","__init__.py"),
-    os.path.join("src","utils.py"),
+    os.path.join(f"src/{project_name}","__init__.py"),
+    os.path.join(f"src/{project_name}","utils.py"),
 
-    os.path.join("src/components","__init__.py"),
-    os.path.join("src/components","data_ingestion.py"),
-    os.path.join("src/components","data_transformation.py"),
-    os.path.join("src/components","model_training.py"),
-    os.path.join("src/components","data_monetring.py"),
+    os.path.join(f"src/{project_name}/components","__init__.py"),
+    os.path.join(f"src/{project_name}/components","data_ingestion.py"),
+    os.path.join(f"src/{project_name}/components","data_transformation.py"),
+    os.path.join(f"src/{project_name}/components","model_training.py"),
+    os.path.join(f"src/{project_name}/components","data_monetring.py"),
 
-    os.path.join("src/pipeline","__init__.py"),
-    os.path.join("src/pipeline","training_pipeline.py"),
-    os.path.join("src/pipeline","prediction_pipeline.py"),
-    "setup.py",
+    os.path.join(f"src/{project_name}/pipeline","__init__.py"),
+    os.path.join(f"src/{project_name}/pipeline","training_pipeline.py"),
+    os.path.join(f"src/{project_name}/pipeline","prediction_pipeline.py"),
     "README.md",
     "requirements.txt",
     ".env"    
 ]
 
 for file in files:
-    with open(file,"w") as f:
-        pass
+    if(not os.path.exists(file)) or (os.path.getsize(file)==0):
 
-
-def require(path):
-    with open(path,"r") as f:
-        print(f.read().splitlines())
-
-require('requirements.txt')
+        with open(file,"w") as f:
+            pass
+    else:
+        logging.info(f"File {file} already exists")
